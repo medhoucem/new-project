1447b40cdaff4aa9883737ba8113b001e2719beb
medhoucem <houcemghrairi@gmail.com>
2021-12-25 11:19:44 +0100

    dd

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const circle = document.querySelector('#circle');
+const circle = document.querySelector('#circle1');
 
 circle.addEventListener('mouseenter',function() {
   if (!circle.classList.contains('hover')) {
@@ -9,5 +9,79 @@ circle.addEventListener('mouseenter',function() {
 circle.addEventListener('mouseleave', () => {
   if (circle.classList.contains('hover')) {
     circle.classList.remove('hover');
+  }
+});
+
+const circle2 = document.querySelector('#circle2');
+
+circle2.addEventListener('mouseenter',function() {
+  if (!circle2.classList.contains('hover')) {
+    circle2.classList.add('hover');
+  }
+});
+
+circle2.addEventListener('mouseleave', () => {
+  if (circle2.classList.contains('hover')) {
+    circle2.classList.remove('hover');
+  }
+});
+
+
+const circle3 = document.querySelector('#circle3');
+
+circle3.addEventListener('mouseenter',function() {
+  if (!circle3.classList.contains('hover')) {
+    circle3.classList.add('hover');
+  }
+});
+
+circle3.addEventListener('mouseleave', () => {
+  if (circle3.classList.contains('hover')) {
+    circle3.classList.remove('hover');
+  }
+});
+
+
+const circle4 = document.querySelector('#circle4');
+
+circle4.addEventListener('mouseenter',function() {
+  if (!circle4.classList.contains('hover')) {
+    circle4.classList.add('hover');
+  }
+});
+
+circle4.addEventListener('mouseleave', () => {
+  if (circle4.classList.contains('hover')) {
+    circle4.classList.remove('hover');
+  }
+});
+
+
+const circle5 = document.querySelector('#circle5');
+
+circle5.addEventListener('mouseenter',function() {
+  if (!circle5.classList.contains('hover')) {
+    circle5.classList.add('hover');
+  }
+});
+
+circle5.addEventListener('mouseleave', () => {
+  if (circle5.classList.contains('hover')) {
+    circle5.classList.remove('hover');
+  }
+});
+
+
+const circle6 = document.querySelector('#circle6');
+
+circle6.addEventListener('mouseenter',function() {
+  if (!circle6.classList.contains('hover')) {
+    circle6.classList.add('hover');
+  }
+});
+
+circle6.addEventListener('mouseleave', () => {
+  if (circle6.classList.contains('hover')) {
+    circle6.classList.remove('hover');
   }
 });
